554aabf03f4b8749b4e940b8028fc31104644046
Temur <38144877+Ascemme@users.noreply.github.com>
2022-04-11 12:29:15 +0300

    init handler

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,1 +1,18 @@
 package handler
+
+import (
+	"github.com/Ascemme/microservice.users.status/pkg/service"
+	"net/http"
+)
+
+type Handler struct {
+	service *service.Service
+}
+
+func NewHandler(service *service.Service) *Handler {
+	return &Handler{service: service}
+}
+
+func (h Handler) GetStatus(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("sssss"))
+}
