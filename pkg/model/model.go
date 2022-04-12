08dd62fe8b3815fd3369b88511b72a81c2fb4461
Temur <38144877+Ascemme@users.noreply.github.com>
2022-04-12 13:38:38 +0300

    init mongoDB

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,1 +1,19 @@
 package model
+
+type User struct {
+	Id        string `json:"id" bson:"_id,omitempty"`
+	Uid       int    `json:"uid" bson:"uid"`
+	Following []int  `json:"following" bson:"following"`
+	Page      []Page `json:"page" bson:"page"`
+}
+type Page struct {
+	Id          int    `json:"id" bson:"id"`
+	Subscribers []int  `json:"subscribers" bson:"subscribers"`
+	Post        []Post `json:"post" bson:"post"`
+}
+type Post struct {
+	Id       int   `json:"id" bson:"id"`
+	Comments []int `json:"comment" bson:"comment"`
+	Likes    []int `json:"likes" bson:"likes"`
+	Dislike  []int `json:"dislike" bson:"dislike"`
+}
--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,8 +1,11 @@
 package service
 
+import "github.com/Ascemme/microservice.users.status/pkg/repository"
+
 type Service struct {
+	repo *repository.Repository
 }
 
-func NewService() *Service {
-	return &Service{}
+func NewService(repo *repository.Repository) *Service {
+	return &Service{repo: repo}
 }
